7b4109d2a2e88a6cfe451a7a1ec592b899b093a2
Mr Fayyaz Khan <45432167+Mr-Khan01@users.noreply.github.com>
2020-11-18 14:35:48 +0000

    changes to the comments

--- HelloPython.py
+++ HelloPython.py
@@ -1,6 +1,8 @@
-//Print date and time
+//the following line of the code will import the current date and time of the system
 import datetime
 print("Hello world!")
 now = datetime.datetime.now()
+
+//this will display the date and the time
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
